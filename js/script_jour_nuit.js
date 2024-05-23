cf484a6eeddacf26329d85bf057970de768a67b7
arthurklr_ <arthur.k2104@gmail.com>
2024-05-23 11:30:01 +0200

    Update script_jour_nuit.js

--- js/script_jour_nuit.js
+++ js/script_jour_nuit.js
@@ -1,12 +1,12 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const icon = document.querySelector('.icon');
     const page = document.querySelector('.page');
     const header = document.querySelector('header')
     const home = document.querySelector('.home')
     const bouton = document.querySelector('button')
-    const bouton2 = document.querySelector('#home > section > div > a:nth-child(2) > button');
+    const bouton2 = document.querySelector('.bouton>a:last-child>button');
 
-    icon.addEventListener('click', function() {
+    icon.addEventListener('click', function () {
         icon.classList.toggle('nuit');
         icon.classList.toggle('jour');
         if (icon.classList.contains('jour')) {
@@ -14,13 +14,13 @@ document.addEventListener('DOMContentLoaded', function() {
             page.classList.add('clair');
             home.classList.add('clair')
             bouton.classList.add('clair')
-            bouton2.classList.add('clair')
+            bouton2.classList.add('bouton_clair')
         } else {
             page.classList.remove('clair');
             header.classList.remove('clair')
             home.classList.remove('clair')
             bouton.classList.remove('clair')
-            bouton2.classList.remove('clair')
+            bouton2.classList.remove('bouton_clair')
         }
     });
 
