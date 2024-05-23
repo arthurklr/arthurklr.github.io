d7fd160f99ef7f952cff708e4c28b8e088ff8745
arthurklr_ <arthur.k2104@gmail.com>
2024-05-23 11:32:18 +0200

    Update script_jour_nuit.js

--- js/script_jour_nuit.js
+++ js/script_jour_nuit.js
@@ -3,8 +3,6 @@ document.addEventListener('DOMContentLoaded', function () {
     const page = document.querySelector('.page');
     const header = document.querySelector('header')
     const home = document.querySelector('.home')
-    const bouton = document.querySelector('button')
-    const bouton2 = document.querySelector('.bouton>a:last-child>button');
 
     icon.addEventListener('click', function () {
         icon.classList.toggle('nuit');
@@ -13,14 +11,10 @@ document.addEventListener('DOMContentLoaded', function () {
             header.classList.add('clair')
             page.classList.add('clair');
             home.classList.add('clair')
-            bouton.classList.add('clair')
-            bouton2.classList.add('bouton_clair')
         } else {
             page.classList.remove('clair');
             header.classList.remove('clair')
             home.classList.remove('clair')
-            bouton.classList.remove('clair')
-            bouton2.classList.remove('bouton_clair')
         }
     });
 
