f3b3118f7b004399f13339e3962910e00519022e
arthurklr_ <arthur.k2104@gmail.com>
2024-05-23 08:40:54 +0200

    Update script_jour_nuit.js

--- js/script_jour_nuit.js
+++ js/script_jour_nuit.js
@@ -1,15 +1,31 @@
 document.addEventListener('DOMContentLoaded', function() {
     const icon = document.querySelector('.icon');
+    const page = document.querySelector('.page');
+    const header = document.querySelector('header')
+    const home = document.querySelector('.home')
+    const bouton = document.querySelector('button')
+    const bouton2 = document.querySelector('.bouton>nth-child(2)')
 
     icon.addEventListener('click', function() {
+        icon.classList.toggle('nuit');
+        icon.classList.toggle('jour');
         if (icon.classList.contains('jour')) {
-            document.querySelector(".page").classList.add("clair")
-            docu
-
+            header.classList.add('clair')
+            page.classList.add('clair');
+            home.classList.add('clair')
+            bouton.classList.add('clair')
         } else {
-            icon.classList.remove('nuit');
-            icon.classList.add('jour');
+            page.classList.remove('clair');
+            header.classList.remove('clair')
+            home.classList.remove('clair')
+            bouton.classList.remove('clair')
         }
     });
-});
 
+    // Vérifier l'état initial de l'icône pour définir l'état de la page
+    if (icon.classList.contains('jour')) {
+        page.classList.add('clair');
+    } else {
+        page.classList.remove('clair');
+    }
+});
