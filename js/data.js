0a8c4d97b099b05cfd2ae3f59bff9da9f655f1e1
arthurklr_ <arthur.k2104@gmail.com>
2024-06-11 10:54:44 +0200

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,5 +1,11 @@
 const projet ={
     "Projet SAÉ": {
-        
+        "SAE1.05":{
+            "titre": "Produire un site web",
+            "description": "En tant qu’intégrateurs ou développeurs juniors dans une agence Web, les étudiants doivent mener un travail de conception, d’intégration et de développement des vues d’un site Web. Le cahier des charges comprend la description des éléments statiques de l’application et un jeu de données structurées représentant un échantillon des contenus à mettre en ligne.",
+        },
+        "SAE2.03":{
+            "titre":
+        },
     }
 }
