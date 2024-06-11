dc06935a744f811bb58deaa4f28a55bd52e1578e
arthurklr_ <arthur.k2104@gmail.com>
2024-06-11 11:06:57 +0200

    Update data.js

--- js/data.js
+++ js/data.js
@@ -4,11 +4,20 @@ const projet ={
             "titre": "Produire un site web",
             "description": "En tant qu’intégrateurs ou développeurs juniors dans une agence Web, les étudiants doivent mener un travail de conception, d’intégration et de développement des vues d’un site Web. Le cahier des charges comprend la description des éléments statiques de l’application et un jeu de données structurées représentant un échantillon des contenus à mettre en ligne.",
             "langage": "HTML, CSS, JavaScript",
+            "apprentissage": "Cette SAÉ m'a permis d'apprendre l'automatisation de la création de pages d'un site web grâce à JavaScript. En effet, les données étaient présentes dans un fichier Json, et nous devions créer des pages fiables et automatiques avec ces données."
         },
         "SAE2.03":{
             "titre": "Concevoir un site web avec une source de données",
             "description": "En tant que développeurs web juniors, les étudiants doivent concevoir un site web relié à une base de données comme par exemple un catalogue de produits organisé par catégories ou mots-clés. Ce site doit présenter de manière lisible et ergonomique les données stockées et permettre de naviguer au sein de celles-ci. Il doit permettre aux utilisateurs d’ajouter des contenus par exemple en postant des commentaires ou en réservant un produit.",
-            "langage": "HTML, CSS, JavaScript, PHP, MySQL"
+            "langage": "HTML, CSS, JavaScript, PHP, MySQL",
+            "apprentissage": "Cette SAÉ m'a permis d'en apprendre plus sur l'utilisation de PHP et de MySQL dans le monde du web. Il était de notre sort de créer un MCD et un MLD pour notre base de données et d'en afficher son contenu sur notre site web",
         },
+    },
+    "Messagerie en ligne":{
+        "titre": "Iris",
+        "description": "Pour un projet personnel, j'ai décidé de me lancer ce défi : créer une messagerie en ligne 100% automatisé grâce à PHP et à MySQL.",
+        "langage": "HTML, CSS, JavaScript, PHP, MySQL",
+        "apprentissage": "Ce projet personnel m'a permis d'approfondir mes connaissances en PHP et de m'imprégnier concrètement des rèègles de sécurité concernant la confidentialité des données ",
+
     }
 }
