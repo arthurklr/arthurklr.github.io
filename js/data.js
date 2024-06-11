89f1c90661a1b3756c4ff33970472f49d3165bf5
arthurklr_ <arthur.k2104@gmail.com>
2024-06-11 11:00:14 +0200

    Update data.js

--- js/data.js
+++ js/data.js
@@ -3,9 +3,12 @@ const projet ={
         "SAE1.05":{
             "titre": "Produire un site web",
             "description": "En tant qu’intégrateurs ou développeurs juniors dans une agence Web, les étudiants doivent mener un travail de conception, d’intégration et de développement des vues d’un site Web. Le cahier des charges comprend la description des éléments statiques de l’application et un jeu de données structurées représentant un échantillon des contenus à mettre en ligne.",
+            "langage": "HTML, CSS, JavaScript",
         },
         "SAE2.03":{
-            "titre":
+            "titre": "Concevoir un site web avec une source de données",
+            "description": "En tant que développeurs web juniors, les étudiants doivent concevoir un site web relié à une base de données comme par exemple un catalogue de produits organisé par catégories ou mots-clés. Ce site doit présenter de manière lisible et ergonomique les données stockées et permettre de naviguer au sein de celles-ci. Il doit permettre aux utilisateurs d’ajouter des contenus par exemple en postant des commentaires ou en réservant un produit.",
+            "langage": "HTML, CSS, JavaScript, PHP, MySQL"
         },
     }
 }
