7a3bd5ab7340ce91c461428bcb44ad98a4d0fb77
arthurklr_ <arthur.k2104@gmail.com>
2024-06-20 09:35:15 +0200

    Update script_jour_nuit_projet.js

--- js/script_jour_nuit_projet.js
+++ js/script_jour_nuit_projet.js
@@ -2,18 +2,21 @@ document.addEventListener('DOMContentLoaded', function () {
     const icon = document.querySelector(".icon")
     const page = document.querySelector(".page")
     const header = document.querySelector("header")
+    const rond = document.querySelector(".rond")
 
 
 
     icon.addEventListener('click', function () {
         icon.classList.toggle('nuit');
         icon.classList.toggle('jour');
-        if(icon.classList.contains('jour')){
+        if (icon.classList.contains('jour')) {
             page.classList.add('clair');
             header.classList.add('clair')
-        } else{
+            rond.classList.add('rond_clair')
+        } else {
             page.classList.remove('clair')
             header.classList.remove('clair')
+            rond.classList.remove('rond_clair')
 
         }
     })
